e3ab6bcaba7b8d6995fe77c82c4b4adbdcf453a9
jack gurulian <jack.gurulian@gmail.com>
2014-05-04 22:01:12 +0300

    Initial commit

---
+++ openwifi/src/main/java/com/org/openwifi/openwifi/Main.java
@@ -0,0 +1,51 @@
+package com.org.openwifi.openwifi;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class Main extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        showDisclaimer();
+    }
+
+    private void showDisclaimer() {
+        AlertDialog.Builder disclaimer = new AlertDialog.Builder(this);
+
+        disclaimer
+                .setIcon(android.R.drawable.ic_dialog_alert).setTitle(getString(R.string.disclaimer))
+                .setMessage(getString(R.string.disc_msg))
+                .setPositiveButton(getString(R.string.ok), new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                    }
+                }).show();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
