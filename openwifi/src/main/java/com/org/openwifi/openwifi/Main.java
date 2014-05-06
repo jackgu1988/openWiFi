4ab853594e21ddbd7e3da02db7307ceb54f7247d
jack gurulian <jack.gurulian@gmail.com>
2014-05-06 14:37:15 +0300

    show disclaimer only on first run

--- openwifi/src/main/java/com/org/openwifi/openwifi/Main.java
+++ openwifi/src/main/java/com/org/openwifi/openwifi/Main.java
@@ -4,30 +4,80 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.text.Html;
+import android.view.ContextThemeWrapper;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.CheckBox;
+
+import static android.content.DialogInterface.BUTTON_POSITIVE;
 
 
 public class Main extends Activity {
+
+    private CheckBox understand;
+    private AlertDialog acceptD;
+    private boolean firstrun;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        showDisclaimer();
+
+        firstrun = getSharedPreferences("PREFERENCE", MODE_PRIVATE).getBoolean("firstrun", true);
+        if (firstrun) {
+            showDisclaimer();
+        }
     }
 
     private void showDisclaimer() {
-        AlertDialog.Builder disclaimer = new AlertDialog.Builder(this);
+        AlertDialog.Builder disclaimer = new AlertDialog.Builder(new ContextThemeWrapper(this, R.style.DiscDialog));
+
+        LayoutInflater accept = LayoutInflater.from(this);
+        View acceptLayout = accept.inflate(R.layout.checkbox, null);
+        assert acceptLayout != null;
+        understand = (CheckBox) acceptLayout.findViewById(R.id.understand);
 
         disclaimer
+                .setView(acceptLayout)
                 .setIcon(android.R.drawable.ic_dialog_alert).setTitle(getString(R.string.disclaimer))
-                .setMessage(getString(R.string.disc_msg))
+                .setMessage(Html.fromHtml(getString(R.string.disc_msg)))
                 .setPositiveButton(getString(R.string.ok), new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
+                        getSharedPreferences("PREFERENCE", MODE_PRIVATE)
+                                .edit()
+                                .putBoolean("firstrun", false)
+                                .commit();
                     }
-                }).show();
+                })
+                .setNegativeButton(getString(R.string.cancel), new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        finish();
+                    }
+                });
+
+        acceptD = disclaimer.create();
+        acceptD.setOnShowListener(new DialogInterface.OnShowListener() {
+
+            @Override
+            public void onShow(DialogInterface dialog) {
+                acceptD.getButton(BUTTON_POSITIVE).setEnabled(false);
+            }
+        });
+        acceptD.setCancelable(false);
+
+        acceptD.show();
+    }
+
+    public void enableOK(View v) {
+        if (!understand.isChecked())
+            acceptD.getButton(BUTTON_POSITIVE).setEnabled(false);
+        else
+            acceptD.getButton(BUTTON_POSITIVE).setEnabled(true);
     }
 
     @Override
