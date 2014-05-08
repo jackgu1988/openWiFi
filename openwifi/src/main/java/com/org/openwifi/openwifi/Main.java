a1c69be87b2912b6d8b001bc1fe5b9a837d9534a
Jack Gurulian <jack.gurulian@gmail.com>
2014-05-08 19:04:57 +0300

    removed unwanted toast

--- openwifi/src/main/java/com/org/openwifi/openwifi/Main.java
+++ openwifi/src/main/java/com/org/openwifi/openwifi/Main.java
@@ -84,8 +84,6 @@ public class Main extends Activity {
         wifiList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> arg0, View arg1, int position, long arg3) {
-                Toast.makeText(getApplicationContext(), "" + position, Toast.LENGTH_SHORT).show();
-
                 connectionDialog(position);
             }
         });
